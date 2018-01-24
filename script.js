644037b003cadd06943ffe8c9528ce64fecffa29
jordanpurinton <jordanomac@gmail.com>
2018-01-23 21:53:23 -0700

    GET json file from github instead of locally for the sake of sanity.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function onLoad() {
-    $.getJSON('capacity_and_booking_lead_time.json', function (data) {
+    $.getJSON('https://raw.githubusercontent.com/jordanpurinton/data-viz/master/capacity_and_booking_lead_time.json', function (data) {
         console.log(JSON.stringify(data));
     });
 }
